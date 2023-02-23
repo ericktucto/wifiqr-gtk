a36eaf2224aa6d228ebc6cb65592eee3f98f301d
Erick Tucto <erick@ericktucto.com>
2023-02-22 22:17:22 -0500

    Simplifying code

--- src/wifi/network_manager.rs
+++ src/wifi/network_manager.rs
@@ -1,51 +1,11 @@
-use std::{ process::{Child, ChildStdout}, io::Read };
+use std::{ process::Child, io::Read };
 use regex::Regex;
 
 use crate::commands::exec;
 
-use super::{Wifi, Lister};
+use super::{Wifi, Lister, show_content, Seccion};
 
 const DIR_NETWORK: &str = "/etc/NetworkManager/system-connections/";
-
-struct Attribute(String, String);
-
-struct Seccion {
-    name: String,
-    attributes: Vec<Attribute>,
-}
-
-impl Seccion {
-    fn make(name: &str, content: &str) -> Self {
-        let re: Regex = Regex::new(r"(?P<nombre>.*)=(?P<valor>.*)\n").unwrap();
-        let mut attributes: Vec<Attribute> = Vec::new();
-        for cap in re.captures_iter(content) {
-            attributes.push(Attribute(cap["nombre"].to_string(), cap["valor"].to_string()));
-        }
-        Self {
-            name: name.to_string(),
-            attributes,
-        }
-    }
-
-    fn get_attribute(&self, name: &str) -> String {
-        for attr in self.attributes.iter() {
-            let Attribute(key, value) = attr;
-            if key == name {
-                return value.clone();
-            }
-        }
-        return String::from("");
-    }
-}
-
-fn show_content(path: String, password: Option<ChildStdout>) -> String {
-    let args = vec!["-S", "-p", "''", "cat", path.as_str()];
-    let resultado = exec("sudo".to_owned(), args, password);
-    let mut output = String::new();
-    resultado.stdout.unwrap().read_to_string(&mut output).unwrap();
-
-    output
-}
 
 pub struct NetworkManager {
     pub password: String
